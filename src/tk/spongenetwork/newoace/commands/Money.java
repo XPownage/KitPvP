9c3742c532db10ff9393e137d9f7364a5874b670
Davin Eastlund <koolkat2100@gmail.com>
2016-04-02 14:50:48 -0500

    Added /money command

--- src/tk/spongenetwork/newoace/commands/Money.java
+++ src/tk/spongenetwork/newoace/commands/Money.java
@@ -1,15 +1,39 @@
 package tk.spongenetwork.newoace.commands;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Money implements CommandExecutor {
 
-    @Override
-    public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
+    File PlayerMoneyYml = new File(
+            Bukkit.getPluginManager().getPlugin("test").getDataFolder() + "/PlayerMoney.yml");
+    FileConfiguration PlayerMoney = YamlConfiguration.loadConfiguration(PlayerMoneyYml);
 
-        return false;
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (sender instanceof Player) {
+            try {
+                PlayerMoney.load(PlayerMoneyYml);
+            } catch (IOException | InvalidConfigurationException e1) {
+                e1.printStackTrace();
+            }
+            Player player = (Player) sender;
+            player.sendMessage(
+                    ChatColor.GOLD + "Money [" + PlayerMoney.getString(player.getName()));
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
