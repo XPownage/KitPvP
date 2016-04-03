b38065c5c6ee87a83e7fe6608a8b66e2a8f2a8e9
Newoace <dadochip@aol.com>
2016-04-02 19:06:37 -0600

    Update: 3

--- src/tk/spongenetwork/newoace/commands/Money.java
+++ src/tk/spongenetwork/newoace/commands/Money.java
@@ -1,39 +1,27 @@
 package tk.spongenetwork.newoace.commands;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
 import java.io.File;
-import java.io.IOException;
 
+/**
+ * Created by Newoace on 4/2/2016.
+ */
 public class Money implements CommandExecutor {
-
-    File PlayerMoneyYml = new File(
-            Bukkit.getPluginManager().getPlugin("test").getDataFolder() + "/PlayerMoney.yml");
-    FileConfiguration PlayerMoney = YamlConfiguration.loadConfiguration(PlayerMoneyYml);
+    File playerMoneyYml = new File(Bukkit.getPluginManager().getPlugin("test").getDataFolder() + "/PlayerMoney.yml");
+    FileConfiguration playerMoneyConfig = YamlConfiguration.loadConfiguration(playerMoneyYml);
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (sender instanceof Player) {
-            try {
-                PlayerMoney.load(PlayerMoneyYml);
-            } catch (IOException | InvalidConfigurationException e1) {
-                e1.printStackTrace();
-            }
-            Player player = (Player) sender;
-            player.sendMessage(
-                    ChatColor.GOLD + "Money [" + PlayerMoney.getString(player.getName()));
-            return true;
-        } else {
-            return false;
+            sender.sendMessage(playerMoneyConfig.getString(sender.getName()));
         }
+        return false;
     }
-
 }
